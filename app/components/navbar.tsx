29019f3cb4c098fb7cbfe42748656dd089b978f5
Shreyas Dutt <151562739+ShreyasDutt@users.noreply.github.com>
2025-05-08 00:19:59 -0700

    NameChange 👅

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className="flex justify-between items-center py-4 bg-[#F7F4ED] border-b border-gray-100 px-2 lg:px-40 border-b-black">
     <Link href="/" className="lg:text-4xl text-3xl font-bold text-gray-900">
-      Medium
+      Typora
     </Link>
     
     <div className="flex items-center gap-6">
